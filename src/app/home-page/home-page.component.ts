8e3aba8936073e9679279761ce53522188e64b58
serhii.rubets <serhii.rubets@playtika.com>
2020-07-07 08:45:05 +0300

    Add main components

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {PostsService} from '../shared/posts.service';
+import {IPost} from '../shared/interfaces';
+import {Observable} from 'rxjs';
 
 @Component({
   selector: 'app-home-page',
@@ -6,10 +9,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home-page.component.scss']
 })
 export class HomePageComponent implements OnInit {
+  posts$: Observable<IPost[]>;
 
-  constructor() { }
+  constructor(private postsService: PostsService) { }
 
   ngOnInit(): void {
+    this.posts$ = this.postsService.getAll();
   }
 
 }
--- src/app/post-page/post-page.component.ts
+++ src/app/post-page/post-page.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import {PostsService} from '../shared/posts.service';
+import {ActivatedRoute, Params} from '@angular/router';
+import {Observable} from 'rxjs';
+import {IPost} from '../shared/interfaces';
+import {switchMap} from 'rxjs/operators';
 
 @Component({
   selector: 'app-post-page',
@@ -6,10 +11,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./post-page.component.scss']
 })
 export class PostPageComponent implements OnInit {
+  post$: Observable<IPost>;
 
-  constructor() { }
+  constructor(private postsService: PostsService, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.post$ = this.route.params.pipe(
+      switchMap((params: Params) => {
+        return this.postsService.getById(params.id);
+      })
+    );
   }
 
 }
